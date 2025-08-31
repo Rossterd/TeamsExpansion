c29281b912f6ea99dfe0b377aeb4989fff0e8839
Ross <2086824-trashp@users.noreply.gitlab.com>
2025-08-31 22:22:56 +0200

    Return empty string when Player is not in a team instead of returning username. Fix #getPlaceholders() returning an empty list. Remove deprecated methods where possible. Simplify code. Fix paper repo address.

--- src/main/java/uk/co/paulcodes/teamsexpansion/TeamsExpansion.java
+++ src/main/java/uk/co/paulcodes/teamsexpansion/TeamsExpansion.java
@@ -2,18 +2,17 @@ package uk.co.paulcodes.teamsexpansion;
 
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.Team;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class TeamsExpansion extends PlaceholderExpansion {
 
-    private static Scoreboard scoreboard = Bukkit.getScoreboardManager().getMainScoreboard();
+    private static final Scoreboard scoreboard = Bukkit.getScoreboardManager().getMainScoreboard();
 
     public boolean canRegister() {
         return true;
@@ -36,99 +35,72 @@ public class TeamsExpansion extends PlaceholderExpansion {
 
     @Override
     public @NotNull String getVersion() {
-        return "0.1-SNAPSHOT";
+        return "0.2-SNAPSHOT";
     }
 
     @Override
     public @NotNull List<String> getPlaceholders() {
-        List<String> list = new ArrayList<>();
-        list.add(plc("color"));
-        list.add(plc("prefix"));
-        list.add(plc("suffix"));
-        list.add(plc("name"));
-        list.add(plc("count"));
-        return super.getPlaceholders();
+        return Arrays.asList(
+                "%team_color%",
+                "%team_count%",
+                "%team_displayname%",
+                "%team_name%",
+                "%team_prefix%",
+                "%team_suffix%",
+                "%team_color_<team>%",
+                "%team_count_<team>%",
+                "%team_displayname_<team>%",
+                "%team_name_<team>%",
+                "%team_prefix_<team>%",
+                "%team_suffix_<team>%"
+        );
     }
 
-    private static String getPlayerTeam(String player) {
-        if (scoreboard.getEntryTeam(player) != null) {
-            return scoreboard.getEntryTeam(player).getName();
-        } else {
-            return "";
+    public String onPlaceholderRequest(Player player, @NotNull String identifier) {
+        String[] args = identifier.toLowerCase().split("_");
+
+        Team team = null;
+        if(args.length == 1) {
+            team = scoreboard.getEntryTeam(player.getName());
+        } else if(args.length == 2) {
+            team = scoreboard.getTeam(args[1]);
         }
+
+        return handle(team, args[0]);
     }
 
-    private static ChatColor getTeamColor(String team) {
-        if (scoreboard.getTeam(team) != null) {
-            return scoreboard.getTeam(team).getColor();
-        } else {
-            return null;
-        }
-    }
-
-    private static String getTeamPrefix(String team) {
-        if (scoreboard.getTeam(team) != null) {
-            return scoreboard.getTeam(team).getPrefix();
-        } else {
-            return "";
-        }
-    }
-
-    private static String getTeamSuffix(String team) {
-        if (scoreboard.getTeam(team) != null) {
-            return scoreboard.getTeam(team).getSuffix();
-        } else {
-            return "";
-        }
-    }
-
-    private static String getDisplayName(String team) {
-        if (scoreboard.getTeam(team) != null) {
-            return scoreboard.getTeam(team).getDisplayName();
-        } else {
-            return "";
-        }
-    }
-
-    private static int getTeamCount(String team) {
-        if (scoreboard.getTeam(team) != null) {
-            return scoreboard.getTeam(team).getSize();
-        } else {
-            return 0;
-        }
-    }
-
-    public String onPlaceholderRequest(Player player, String identifier) {
-        if (getPlayerTeam(player.getName()).isEmpty()) {
-            if (identifier.contains("count") && identifier.contains("team")) {
-                String teamName = identifier.split("_")[2];
-                if (teamName.isEmpty()) {
-                    return "0";
-                } else {
-                    return getTeamCount(teamName) + "";
-                }
-            } else {
-                return player.getName();
+    // Return empty string if valid placeholder, but team or specific value does not exist.
+    // Return null if placeholder is invalid. This makes the placeholder return itself.
+    String handle(Team team, String placeholder) {
+        try {
+            switch (placeholder) {
+                case "color":
+                    if (team == null) return "";
+                    // Somewhat stuck to using deprecated method as this returns a legacy color character.
+                    // A modern solution would be returning a MiniMessage tag, but that would probably break a lot
+                    // of plugins.
+                    return team.getColor().toString();
+                case "prefix":
+                    if (team == null) return "";
+                    return team.prefix().toString();
+                case "suffix":
+                    if (team == null) return "";
+                    return team.suffix().toString();
+                case "name":
+                    if (team == null) return "";
+                    return team.getName();
+                case "displayname":
+                    if (team == null) return "";
+                    return team.displayName().toString();
+                case "count":
+                    if (team == null) return "";
+                    return String.valueOf(team.getSize());
+                default:
+                    return null;
             }
-        } else {
-            if (identifier.contains("color")) {
-                return getTeamColor(getPlayerTeam(player.getName())) + "";
-            } else if (identifier.contains("prefix")) {
-                return getTeamPrefix(getPlayerTeam(player.getName()));
-            } else if (identifier.contains("suffix")) {
-                return getTeamSuffix(getPlayerTeam(player.getName()));
-            } else if (identifier.contains("name")) {
-                return getPlayerTeam(player.getName());
-            } else if (identifier.contains("displayname")) {
-                return getDisplayName(getPlayerTeam(player.getName()));
-            } else if (identifier.contains("count")) {
-                return getTeamCount(getPlayerTeam(player.getName())) + "";
-            }
+        } catch(IllegalStateException e) {
+            // Value not set for team
+            return "";
         }
-        return null;
-    }
-
-    private String plc(String str) {
-        return "%" + getIdentifier() + "_" + str + "%";
     }
 }
